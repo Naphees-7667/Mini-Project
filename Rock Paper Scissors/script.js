e95bebafc92803383d2ee3183bd00f74c1acddc5
Naphees-7667 <naphees6200@gmail.com>
2024-06-03 18:18:23 +0530

    commit changes in game code

--- Rock Paper Scissors/script.js
+++ Rock Paper Scissors/script.js
@@ -1,7 +1,35 @@
-/* const playGame = (userChoice) => {
-  //Generate computer choice
-  const compChoice = genCompChoice();
+let userScore = 0;
+let compScore = 0;
 
+const choice = document.querySelectorAll(".choice");
+const User = document.getElementById('user-score');
+const Comp = document.getElementById('comp-score');
+const result = document.querySelector('#msg');
+
+const choiceArr = ["rock", "paper", "scissors"];
+
+const drawGame = () => {
+  result.innerText = `Game Draw`;
+}
+
+const msg = (message) => {
+  User.innerText = userScore;
+  Comp.innerText = compScore;
+  result.innerText = message;
+}
+
+const showWinner = (userWin, userChoice, compChoice) => {
+  if(userWin){
+    userScore++;
+    msg(`You Won! Your ${userChoice} beats ${compChoice}`)
+  }
+  else {
+    compScore++;
+    msg(`You Loose! Play Again`)
+  }
+}
+
+const playGame = (userChoice, compChoice) => {
   if (userChoice === compChoice) {
     //Draw Game
     drawGame();
@@ -17,9 +45,21 @@
       //rock, paper
       userWin = compChoice === "rock" ? false : true;
     }
+
     showWinner(userWin, userChoice, compChoice);
   }
 };
-*/
 
+function compChoiceFun() {
+  let randomIndex = Math.floor(Math.random() * 3);
+  let compChoice = choiceArr[randomIndex];
+  return compChoice;
+}
 
+choice.forEach((uChoice) => {
+  uChoice.addEventListener("click", (e) => {
+    let userChoice = uChoice.getAttribute("id");
+    let compChoice = compChoiceFun();
+    playGame(userChoice, compChoice);
+  });
+});
