7d2827c03f3e3b8205980aa3ad1abccc1a46c9f1
Naphees-7667 <naphees6200@gmail.com>
2024-06-03 10:44:31 +0530

    random number guessing game

---
+++ Rock Paper Scissors/script.js
@@ -0,0 +1,25 @@
+/* const playGame = (userChoice) => {
+  //Generate computer choice
+  const compChoice = genCompChoice();
+
+  if (userChoice === compChoice) {
+    //Draw Game
+    drawGame();
+  } else {
+    let userWin = true;
+    if (userChoice === "rock") {
+      //scissors, paper
+      userWin = compChoice === "paper" ? false : true;
+    } else if (userChoice === "paper") {
+      //rock, scissors
+      userWin = compChoice === "scissors" ? false : true;
+    } else {
+      //rock, paper
+      userWin = compChoice === "rock" ? false : true;
+    }
+    showWinner(userWin, userChoice, compChoice);
+  }
+};
+*/
+
+
