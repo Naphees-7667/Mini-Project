4dbf29352af5262f5e31ad8ac593475d1693d084
Naphees-7667 <naphees6200@gmail.com>
2024-06-11 18:03:03 +0530

    Todo App Created in Html Css And Javascript

---
+++ Todo App/script.js
@@ -0,0 +1,41 @@
+const todoContainer = document.getElementById("allTodo")
+
+const btn = document.querySelector("#addBtn")
+
+const todoInput = document.querySelector("#todoInput")
+
+
+const addTodo = (value) => {
+
+    const newElement = document.createElement('div');
+    newElement.setAttribute('class','singleTodo')
+
+    newElement.innerHTML = `<h2>${value}</h2>
+            <input type="checkbox" name="" id="">
+            <button>Remove Todo</button>`;
+
+    const buttonEle = newElement.getElementsByTagName('button')
+
+    buttonEle[0].addEventListener('click', (evt) => {
+        evt.target.parentNode.remove();
+    })
+
+    const inputButton = newElement.getElementsByTagName('input')
+
+    inputButton[0].addEventListener('click', (evt) => {
+        const list = evt.target.previousElementSibling.classList;
+        list.toggle("checked")
+    })
+
+    todoContainer.appendChild(newElement);
+    todoInput.value = ""
+}
+
+btn.addEventListener('click' , (evt) => {
+    evt.preventDefault()
+
+    const value = todoInput.value;
+
+    addTodo(value)
+
+})
